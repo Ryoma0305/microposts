2afb0a2619a953114f9eb1b6bede18b296db6045
ryoma <techacademy.ryoma@gmail.com>
2020-02-29 03:13:26 +0000

    post

--- app/Http/Controllers/MicropostsController.php
+++ app/Http/Controllers/MicropostsController.php
@@ -21,4 +21,28 @@ class MicropostsController extends Controller
         
         return view('welcome', $data);
     }
+    
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'content' => 'required|max:191',
+        ]);
+
+        $request->user()->microposts()->create([
+            'content' => $request->content,
+        ]);
+
+        return back();
+    }
+    
+    public function destroy($id)
+    {
+        $micropost = \App\Micropost::find($id);
+
+        if (\Auth::id() === $micropost->user_id) {
+            $micropost->delete();
+        }
+
+        return back();
+    }
 }
