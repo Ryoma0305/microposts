8f768adc5f9e94383d2dd6f83fc60c764e090ff7
ryoma <techacademy.ryoma@gmail.com>
2020-03-08 14:51:47 +0000

    add-favorites

---
+++ resources/views/favorites/favorites_button.blade.php
@@ -0,0 +1,9 @@
+    @if (Auth::user()->is_favorite($micropost->id))
+        {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
+            {!! Form::submit('Unfavorite', ['class' => "btn btn-danger btn-sm mr-2"]) !!}
+        {!! Form::close() !!}
+    @else
+        {!! Form::open(['route' => ['favorites.favorite', $user->id]]) !!}
+            {!! Form::submit('Favorite', ['class' => "btn btn-success btn-sm mr-2"]) !!}
+        {!! Form::close() !!}
+    @endif
---
+++ resources/views/users/favorites.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <aside class="col-sm-4">
+            @include('users.card', ['user' => $user])
+        </aside>
+        <div class="col-sm-8">
+            @include('users.navtabs', ['user' => $user])
+            
+            <ul class="list-unstyled">
+                @foreach ($microposts as $micropost)
+                    <li class="media mb-3">
+                        <img class="mr-2 rounded" src="{{ Gravatar::src($micropost->email, 50) }}" alt="">
+                        <div class="media-body">
+                            <div>
+                                {!! link_to_route('users.show', $micropost->name, ['id' => $micropost->id]) !!} <span class="text-muted">posted at {{ $micropost->created_at }}</span>
+                            </div>
+                            <div>
+                                <p class="mb-0">{!! nl2br(e($micropost->content)) !!}</p>
+                            </div>
+                            @include('favorites.favorites_button', ['micropost' => $micropost])
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
+            {{ $microposts->links('pagination::bootstrap-4') }}
+        </div>
+    </div>
+@endsection
